55479e17932dce039fc2262fea67ed560b6111c4
Darakhshan08 <darakhshanshah8@gmail.com>
2025-12-19 15:40:24 +0500

    update docusaurus config
    update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,16 +12,19 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Darakhshan08.github.io',
+  // url: 'https://Darakhshan08.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  // ✅ Vercel production URL
+  url: 'https://physical-ai-robotics-book-inky.vercel.app',
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Darakhshan08',
-  projectName: 'Physical-AI-Humanoid-Robotics-Book',
-  deploymentBranch: 'gh-pages', // The branch Docusaurus will push to for deployment.
+  // organizationName: 'Darakhshan08',
+  // projectName: 'Physical-AI-Humanoid-Robotics-Book',
+  // deploymentBranch: 'gh-pages', // The branch Docusaurus will push to for deployment.
+  
   trailingSlash: false, // For GitHub Pages
 
   onBrokenLinks: 'warn',
